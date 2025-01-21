79fde569d64bec7423b7eb5eb3bea699a286ea17
Prajna Majumdar <100031258+Prajnaaaa@users.noreply.github.com>
2025-01-21 14:24:53 +0530

    asgi.py

---
+++ chatapp/Update chatapp/asgi.py
@@ -0,0 +1,12 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
